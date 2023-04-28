6a60af4359b0f2e257c9465ecc4c5599957e901a
Rusiru Herath <it21002106@my.sliit.lk>
2023-04-28 22:51:17 +0530

    Create post controller & Repo

---
+++ foodies/src/main/java/com/project/foodies/PostManagement/PostController.java
@@ -0,0 +1,71 @@
+package com.project.foodies.PostManagement;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController 
+@RequestMapping(path = "/paf") 
+public class PostController {
+    @Autowired
+    private PostRepository postRepository;
+
+    @PostMapping(path = "/addpost")  
+    public @ResponseBody String addNewPost(@RequestParam String restaurantName, @RequestParam String experience, @RequestParam("photo") MultipartFile photo) throws IOException {
+       
+       
+       
+        Post n = new Post();
+        n.setRestaurantName(restaurantName);
+        n.setExperience(experience);
+        n.setPhoto(photo.getBytes());
+        postRepository.save(n);
+        return "Post Saved";
+    }
+
+    @GetMapping(path = "/allpost")
+    public @ResponseBody Iterable<Post> getAllUsers() {
+        
+        return postRepository.findAll();
+    }
+
+    
+    @PutMapping(path = "/updatepost/{id}")
+    public @ResponseBody String updatePost(@PathVariable Integer id, @RequestParam String restaurantName, @RequestParam String experience, @RequestParam("photo") MultipartFile photo) throws IOException {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            post.setRestaurantName(restaurantName);
+            post.setExperience(experience);
+            post.setPhoto(photo.getBytes());
+            postRepository.save(post);
+            return "Updated";
+        } else {
+            return "not found";
+        }
+    }
+
+
+    @DeleteMapping(path = "/deletepost/{id}")
+    public @ResponseBody String deletePost(@PathVariable Integer id) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if (optionalPost.isPresent()) {
+            postRepository.deleteById(id);
+            return "Deleted";
+        } else {
+            return "not found";
+        }
+    }
+
+}
---
+++ foodies/src/main/java/com/project/foodies/PostManagement/PostRepository.java
@@ -0,0 +1,9 @@
+package com.project.foodies.PostManagement;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Integer> {
+
+}
